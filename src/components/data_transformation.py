aae3e298ea68e507075273e4271d40e23590d40c
Muhammad ikram <ikram2500@gmail.com>
2024-11-27 20:53:11 +0500

    data transformation done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,7 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer 
 from sklearn.pipeline import Pipeline 
 from sklearn.preprocessing import OneHotEncoder 
-from sklearn.preprocessing import StandardScaler 
+from sklearn.preprocessing import StandardScaler
 
 from src.exception import CustomException 
 from src.logger import logging 
